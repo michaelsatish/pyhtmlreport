cad9a4c6afc3e3a6835b26b949978ae295fd9395
Satish Kumar Kadarkarai Mani <michael.satish@gmail.com>
2017-12-30 19:52:57 -0500

    used pytest fixtures

--- tests/test_report_advanced.py
+++ tests/test_report_advanced.py
@@ -7,40 +7,46 @@ from pyhtmlreport import Report
 
 @pytest.fixture(scope='function')
 def temp_dir(tmpdir_factory):
-    td = tmpdir_factory.mktemp('download')
+    td = tmpdir_factory.mktemp('reports')
     yield td
 
 
-def test_setup(temp_dir):
+@pytest.fixture(scope='function')
+def tr():
     r = Report()
-    root_report_folder = os.path.join(temp_dir, 'Reports')
-    r.setup(root_report_folder, module_name='Test Module')
-    assert os.path.exists(root_report_folder)
-
-    assert 'Test Module' in os.listdir(root_report_folder)[0]
-    assert 'Capture' in os.listdir(os.path.join(root_report_folder, os.listdir(root_report_folder)[0]))[0]
+    yield r
 
 
-def test_capture_screenshot(temp_dir):
-    r = Report()
-    root_report_folder = os.path.join(temp_dir, 'Reports')
-    r.setup(root_report_folder, module_name='Test Screenshot')
+class TestReport:
 
-    capture_folder = os.path.join(root_report_folder, os.listdir(root_report_folder)[0], 'Capture')
-    r.capture_screenshot()
-    assert len(os.listdir(capture_folder)) == 1
+    def test_setup(self, temp_dir, tr):
+        root_report_folder = os.path.join(temp_dir, 'Test Reports')
+        tr.setup(root_report_folder, module_name='Test Module')
+        assert os.path.exists(root_report_folder)
 
+        module_folder = os.listdir(root_report_folder)[0]
+        assert 'Test Module' in module_folder
+        assert 'Capture' in os.listdir(os.path.join(root_report_folder, module_folder))[0]
 
-def test_generate_report(temp_dir):
-    r = Report()
-    root_report_folder = os.path.join(temp_dir, 'Reports')
-    r.setup(root_report_folder, module_name='Test Module', release_name='Test Release')
-    r.write_step('Test', r.status.Start)
-    r.write_step('Test Passed', r.status.Pass, screenshot=True)
-    r.write_step('Test Failed', r.status.Fail, screenshot=True)
-    r.write_step('Test Warning', r.status.Fail, screenshot=True)
-    r.generate_report()
-    assert 'Report.html' in os.listdir(os.path.join(root_report_folder, os.listdir(root_report_folder)[0]))
+    def test_capture_screenshot(self, temp_dir, tr):
+        root_report_folder = os.path.join(temp_dir, 'Test Reports')
+        tr.setup(root_report_folder, module_name='Test Module')
+
+        capture_folder = os.path.join(root_report_folder, os.listdir(root_report_folder)[0], 'Capture')
+        tr.capture_screenshot()
+        assert len(os.listdir(capture_folder)) == 1
+
+    def test_generate_report(self, temp_dir, tr):
+        root_report_folder = os.path.join(temp_dir, 'Test Reports')
+        tr.setup(root_report_folder, module_name='Test Module')
+
+        tr.write_step('Test', tr.status.Start)
+        tr.write_step('Test Passed', tr.status.Pass, screenshot=True)
+        tr.write_step('Test Failed', tr.status.Fail, screenshot=True)
+        tr.write_step('Test Warning', tr.status.Fail, screenshot=True)
+
+        assert 'Test Module' in tr.generate_report()
+        assert 'Report.html' in os.listdir(os.path.join(root_report_folder, os.listdir(root_report_folder)[0]))
 
 
 if __name__ == '__main__':
