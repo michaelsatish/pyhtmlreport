4b34c59b8971e0f89bd95cb84228c9f8ac91fdf8
Satish Kumar Kadarkarai Mani <michael.satish@gmail.com>
2018-10-28 20:43:23 -0400

    Dependencies updated

--- setup.py
+++ setup.py
@@ -5,8 +5,11 @@ setup(
 	version='1.0',
 	packages=['pyhtmlreport'],
 	package_data = {'pyhtmlreport': ['data/*']},
-	description='A python library to implement html reporting',
+	description='python library to implement html reporting',
 	author='Satish Kumar Kadarkarai Mani',
 	author_email='michael.satish@gmail.com',
-	install_requires=['Pillow>=4.3.0']
+	install_requires=[
+		'Pillow>=5.3.0',
+		'jinja2>=2.10'
+		]
 	)
