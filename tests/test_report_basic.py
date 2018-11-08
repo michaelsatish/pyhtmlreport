9b4cce67e6a5259dc30a4feb524dcd62589c3c96
Satish Kumar Kadarkarai Mani <michael.satish@gmail.com>
2018-11-07 19:24:24 -0500

    updated basic tests

--- tests/test_report_basic.py
+++ tests/test_report_basic.py
@@ -1,37 +1,13 @@
 import types
 
 import pytest
-
+from jinja2 import Template
 from pyhtmlreport import Report
-from pyhtmlreport.client import dispatch_screenshot_number, update_test_status
+from pyhtmlreport.report import Step, Test, Tests, dispatch_screenshot_number
 
 
 def test_dispatch_screenshot_number_return_type():
-    assert isinstance(dispatch_screenshot_number(), types.GeneratorType)
-
-
-def test_update_test_status_failed():
-    assert [0, 1, 0] == update_test_status(0, 1, 0)
-    assert [0, 1, 0] == update_test_status(1, 1, 0)
-    assert [0, 1, 0] == update_test_status(0, 1, 1)
-    assert [0, 1, 0] == update_test_status(1, 1, 1)
-    assert [0, 1, 0] == update_test_status(2, 2, 2)
-
-
-def test_update_test_status_warning():
-    assert [0, 0, 1] == update_test_status(0, 0, 1)
-    assert [0, 0, 1] == update_test_status(1, 0, 1)
-    assert [0, 0, 1] == update_test_status(0, 0, 2)
-    assert [0, 0, 1] == update_test_status(2, 0, 2)
-    assert [0, 0, 1] != update_test_status(1, 1, 1)
-
-
-def test_update_test_status_passed():
-    assert [1, 0, 0] == update_test_status(1, 0, 0)
-    assert [1, 0, 0] == update_test_status(2, 0, 0)
-    assert [1, 0, 0] == update_test_status(3, 0, 0)
-    assert [1, 0, 0] != update_test_status(3, 0, 1)
-    assert [1, 0, 0] != update_test_status(3, 1, 0)
+    assert isinstance(dispatch_screenshot_number(1000), types.GeneratorType)
 
 
 def test_screenshot_num():
@@ -41,15 +17,67 @@ def test_screenshot_num():
     assert 1 == next(r.screenshot_num)
 
 
-def test_html():
+def test_template_type():
     r = Report()
-    assert isinstance(r.html, str)
+    assert isinstance(r.report_template, Template)
 
 
-def test_selenium_webdriver_plug():
-    r = Report()
-    assert not r.selenium_driver
+def test_type_error():
+    with pytest.raises(TypeError):
+        ts = Tests()
+        ts.append('this is not a test type')
 
 
-if __name__ == '__main__':
-    pytest.main()
+def test_append():
+    t = Test(1, 'This is a Test')
+    ts = Tests()
+    ts.append(t)
+    assert len(ts) == 1
+
+
+def test_pass_status():
+    step1 = Step('This is Step1', 'Pass', None)
+    step2 = Step('This is Step2', 'Pass', None)
+    step3 = Step('This is Step2', 'Pass', None)
+
+    t = Test(1, 'This is a Test')
+    t.steps.append(step1)
+    t.steps.append(step2)
+    t.steps.append(step3)
+
+    ts = Tests()
+    ts.append(t)
+    tr = ts[0]
+    assert tr.status == 'Pass'
+
+
+def test_fail_status():
+    step1 = Step('This is Step1', 'Pass', None)
+    step2 = Step('This is Step2', 'Fail', None)
+    step3 = Step('This is Step2', 'Warn', None)
+
+    t = Test(1, 'This is a Test')
+    t.steps.append(step1)
+    t.steps.append(step2)
+    t.steps.append(step3)
+
+    ts = Tests()
+    ts.append(t)
+    tr = ts[0]
+    assert tr.status == 'Fail'
+
+
+def test_warn_status():
+    step1 = Step('This is Step1', 'Pass', None)
+    step2 = Step('This is Step2', 'Pass', None)
+    step3 = Step('This is Step2', 'Warn', None)
+
+    t = Test(1, 'This is a Test')
+    t.steps.append(step1)
+    t.steps.append(step2)
+    t.steps.append(step3)
+
+    ts = Tests()
+    ts.append(t)
+    tr = ts[0]
+    assert tr.status == 'Warn'
